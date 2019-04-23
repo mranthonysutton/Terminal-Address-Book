c8095717fdb45ca57c5ab887ac5cd43e2dfbc8dc
mranthonysutton <anthony.sutton95@gmail.com>
2019-04-23 08:05:28 -0700

    Add files via upload

--- addressbook.py
+++ addressbook.py
@@ -1,86 +1,62 @@
-import pickle
+import csv
 import os
 
-# contact_list = [
-#     {'name': 'Aimee Brunelle', 'email': 'ai-brunelle@arvinmeritor.info',
-#         'phone': '862-467-5121'},
-#     {'name': 'Stockley Meiners',
-#         'email': 'stokley-meine@arvinmeritor.info', 'phone': '867-651-4298'},
-#     {'name': 'Elmer Wilcox', 'email': 'elmerwillcox@autozone-inc.info',
-#         'phone': '808-433-4285'},
-#     {'name': 'Lainey Amezcua',
-#      'email': 'lainey_am@arvinmeritor.info', 'phone': '867-319-2418'}
-# ]
+# Prints out all of the contacts contained in the csv file to the terminal
+def view_contacts():
+    # Checks if the file exists, if not, directs the user to add contacts
+    if os.path.isfile('contacts.csv'):
+        with open('contacts.csv', 'r', newline='') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            print('\nName:\t\tPhone:\t\tEmail:')
 
-contactdata = 'contacts.txt'
-# f = open(contactdata, 'wb')
-# pickle.dump(contact_list, f)
-# f.close()
+            for line in csv_reader:
+                print(line['name'], '\t', line['phone'], '\t', line['email'])
+    else:
+        print("Sorry you do not have any contacts. Let's add some now!\n")
+        add_contacts()
 
-class Contact:
-    def __init__(self, name, email, phone):
-        self.name = name
-        self.email = email
-        self.phone = phone
+# Asks the user a series of questions to add the contact to their address book
+def add_contacts():
+    print('\n')
+    row = []
 
-    def print_contacts(self):
-        # Checks if the contacts file exists, then prints the contacts
-        if os.path.exists(contactdata):
-            # Checks to see if contacts even exist within the text file
-            if os.path.getsize(contactdata) <= 0:
-                print("You have no contacts.")
-            else:
-                print('Name:\t\t\tEmail:\t\t\tPhone:')
+    # Checks if the file exists then asks the user the questions
+    # needed to import the contact information
+    # If the file does not exist, creates a new file with the required headers
+    if os.path.isfile('contacts.csv'):
 
-                f = open(contactdata, 'rb')
-                # Load the object from the file
-                contactList = pickle.load(f)
+        row.append(input("What is the name: "))
+        row.append(input("What is the phone number: "))
+        row.append(input("What is the email address: "))
 
-                for contact in contactList:
-                    print(contact['name'], '\t', contact['email'],
-                        '\t', contact['phone'])
-        else:
-            print("Sorry, but you do not have any contacts. Please add some now:\n")
-            f = open(contactdata, 'wb')
-            f.close()
+        with open('contacts.csv', 'a', newline='') as csv_file:
+            headers = ['name', 'phone', 'email']
+            writer = csv.writer(csv_file)
+            writer.writerow(row)
 
-    def add_contacts(self):
-        # Opens contact text file and asks for name, email, and phone number
-        f = open(contactdata, 'rb')
-        contactList = pickle.load(f)
-        self.name = input("What is your full name: ")
-        self.email = input("What is your email: ")
-        self.phone = input("What is your phone number: ")
+        csv_file.close()
+    else:
+        row.append(input("What is the name: "))
+        row.append(input("What is the phone number: "))
+        row.append(input("What is the email address: "))
 
-        # Adds the answers from the user into a list
-        for i in range(1):
-            d = {}
-            d['name'] = self.name
-            d['email'] = self.email
-            d['phone'] = self.phone
-            contactList.append(d)
-        
-        # Writes the next list to the contact file
-        f.close()
-        f = open(contactdata, 'wb')
-        pickle.dump(contactList, f)
-        f.close()
+        with open('contacts.csv', 'a', newline='') as csv_file:
+            headers = ['name', 'phone', 'email']
+            writer = csv.writer(csv_file)
+            writer.writerow(headers)
+            writer.writerow(row)
 
+        csv_file.close()
 
+    view_contacts()
+
+# Program Interface
 print("What would you like to do: \n")
 prompt = input("(V)iew contacts\n(A)dd new contact\n").lower()
 
-c = Contact('Aimee Brunelle', 'ai-brunelle@arvinmeritor.info', '862-467-5121')
-
 if prompt == 'v':
-    c = Contact('Anthony Sutton', 'anthonysutton.io', '702-886-7493')
-    print()
-    c.print_contacts()
+    view_contacts()
 elif prompt == 'a':
-    c = Contact('Anthony Sutton', 'anthonysutton.io', '702-886-7493')
-    print()
-    c.add_contacts()
-    print()
-    c.print_contacts()
+    add_contacts()
 else:
     print("Sorry, but the program has ended due to wrong input.")
